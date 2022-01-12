2d09dafb4b26f40e86bba7562203806a0c4ded76
abv76 <91702855+abv76@users.noreply.github.com>
2022-01-12 10:57:21 +0000

    week-19 HW

---
+++ src/main/java/com/studentapp/constants/Path.java
@@ -0,0 +1,15 @@
+package com.studentapp.constants;
+
+/**
+ * Created by Jay
+ */
+public class Path {
+    /*
+     * This is base path for student api
+     */
+    public static final String STUDENT = "/student";
+
+
+
+
+}
